4f94e41984eea5e5d5e4b7ddabd13b5e87dd616e
nataliepo <natalie@29.io>
2014-02-20 13:03:41 -0500

    adding real support form

--- media/tmpl/support.py
+++ media/tmpl/support.py
@@ -6,6 +6,10 @@
     <h1>support</h1>
 </div>
 
+<div class="rsvp-form">
+    <iframe src="https://docs.google.com/forms/d/1LId7q5UDFA56XfrU7rxMRV9v6ZbxrgHLpJlE7FXYwWo/viewform?embedded=true" width="400" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...</iframe>
+</div>
+
 #end def
 
 
